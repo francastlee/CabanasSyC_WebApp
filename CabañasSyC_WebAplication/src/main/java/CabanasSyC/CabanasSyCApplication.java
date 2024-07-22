bc72ebf0621e811049938a400fb20938d5c0e5d0
FranklinU22 <133930634+FranklinU22@users.noreply.github.com>
2024-07-21 19:31:12 -0600

    initialCommit
    I did all the settings to start to develop the project.

---
+++ CabañasSyC_WebAplication/src/main/java/CabanasSyC/CabanasSyCApplication.java
@@ -0,0 +1,13 @@
+package CabanasSyC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabanasSyCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabanasSyCApplication.class, args);
+	}
+
+}
---
+++ CabañasSyC_WebAplication/src/test/java/CabanasSyC/CabanasSyCApplicationTests.java
@@ -0,0 +1,13 @@
+package CabanasSyC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabanasSyCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
