fb1c60d9cd2caa77025249a504f095153ecf6cf9
FranklinU22 <133930634+FranklinU22@users.noreply.github.com>
2024-08-06 16:24:14 -0600

    feature/createNavbar
    The navbarf was developed and prepared to be responsive.

---
+++ CabañasSyC_WebAplication/src/main/resources/static/js/menuResponsive.js
@@ -0,0 +1,12 @@
+function onToggleMenu(e) {
+    const navLiks = document.querySelector('.linksSelector');
+    const menuSelector = document.querySelector('.menuSelector');
+    if (menuSelector.classList.contains('fa-bars')) {
+        menuSelector.classList.remove('fa-bars');
+        menuSelector.classList.add('fa-xmark');
+    } else {
+        menuSelector.classList.remove('fa-xmark');
+        menuSelector.classList.add('fa-bars');
+    }
+    navLiks.classList.toggle('top-[-65%]');
+}
